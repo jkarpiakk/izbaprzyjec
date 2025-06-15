2a5355d63b25f888908701c47f0beb91fbe5a665
jkarpiakk <jakub.karpiak@gmail.com>
2025-06-15 17:08:22 +0200

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -2,6 +2,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { firebaseConfig } from './firebaseConfig';
-let app = getApps()[0] || initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+let app;
+if (typeof window !== 'undefined') {
+  app = !getApps().length
+    ? initializeApp(firebaseConfig)
+    : getApps()[0];
+}
+
+export const auth = typeof window !== 'undefined' && app ? getAuth(app) : null;
+export const db = typeof window !== 'undefined' && app ? getFirestore(app) : null;
