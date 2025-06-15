ca59b7097dda6fea8eb8b0d3792ad105df4e4993
jkarpiakk <jakub.karpiak@gmail.com>
2025-06-15 14:02:14 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',
 };
-module.exports = nextConfig;
