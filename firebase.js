e1c3b7083a850ca3ecee02aabcc2338694b8e47e
jkarpiakk <jakub.karpiak@gmail.com>
2025-06-15 13:56:27 +0200

    Delete firebase.js

--- firebase.js
+++
@@ -1,8 +0,0 @@
-import { initializeApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { firebaseConfig } from "./firebaseConfig";
-
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
-export const auth = getAuth(app);
-export const db = getFirestore(app);
