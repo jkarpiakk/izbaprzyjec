b4888cb06152307c41e9f64b908a98fe151751ab
jkarpiakk <jakub.karpiak@gmail.com>
2025-06-15 13:55:46 +0200

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout';
-
-export default function Home() {
-  return (
-    <Layout>
-      <h1 className="text-3xl font-bold text-center">Izba Przyjęć</h1>
-    </Layout>
-  );
-}
--- pages/register.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout';
-
-export default function Register() {
-  return (
-    <Layout>
-      <h2 className="text-2xl font-semibold text-center">Rejestracja</h2>
-    </Layout>
-  );
-}
